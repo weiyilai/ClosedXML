f31d4dc17bc3a6e5c3b8ef32ea60f16373d86c8d
jahav <havlicek.honza@gmail.com>
2026-02-19 21:31:02 +0100

    Replace old style structures in pivot format

--- ClosedXML/Excel/IO/PivotTableDefinitionPartReader.cs
+++ ClosedXML/Excel/IO/PivotTableDefinitionPartReader.cs
@@ -146,32 +146,15 @@ internal class PivotTableDefinitionPartReader
             foreach (var format in formats.Cast<Format>())
             {
                 var action = format.Action?.Value.ToClosedXml() ?? XLPivotFormatAction.Formatting;
-                var dxfKey = XLStyleValue.Default.Key;
-                if (format.FormatId is not null)
-                {
-                    // TODO: What about alignment?
-                    var differentialFormats = sheet.Workbook.Styles.DifferentialFormats;
-                    var df = differentialFormats[checked((int)format.FormatId.Value)];
-                    if (df.NumberFormat is not null)
-                        dxfKey = dxfKey with { NumberFormat = XLNumberFormatKey.ForFormat(df.NumberFormat) };
-
-                    if (df.Font is not null)
-                        dxfKey = dxfKey with { Font = df.Font.ApplyTo(dxfKey.Font) };
-
-                    if (df.Fill is not null)
-                        dxfKey = dxfKey with { Fill = df.Fill.ApplyTo(dxfKey.Fill) };
-
-                    if (df.Border is not null)
-                        dxfKey = dxfKey with { Border = df.Border.ApplyTo(dxfKey.Border) };
-                }
-
-                var dxfValue = XLStyleValue.FromKey(ref dxfKey);
+                var dxf = format.FormatId is { } dxfId
+                    ? sheet.Workbook.Styles.DifferentialFormats[checked((int)dxfId.Value)]
+                    : null;
                 var pivotArea = format.PivotArea ?? throw PartStructureException.ExpectedElementNotFound();
                 var xlPivotArea = LoadPivotArea(pivotArea);
                 var xlFormat = new XLPivotFormat(xlPivotArea)
                 {
                     Action = action,
-                    DxfStyleValue = dxfValue,
+                    FormatValue = dxf,
                 };
                 xlPivotTable.AddFormat(xlFormat);
             }
