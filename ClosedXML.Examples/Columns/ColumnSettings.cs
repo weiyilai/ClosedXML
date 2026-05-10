4feda378f3222b4c499492ea135b4b2139265832
jahav <havlicek.honza@gmail.com>
2026-05-10 16:24:14 +0200

    Update example test file ColumnSettings
    Update reference test file for example ColumnSettings. After
    style infrastructure switch, the styles part is saved differently.
    No semantic change to styles part.

--- ClosedXML.Examples/Columns/ColumnSettings.cs
+++ ClosedXML.Examples/Columns/ColumnSettings.cs
@@ -1,59 +1,10 @@
 using System;
 using ClosedXML.Excel;
 
-
 namespace ClosedXML.Examples.Columns
 {
     public class ColumnSettings : IXLExample
     {
-        #region Variables
-
-        // Public
-
-        // Private
-
-
-        #endregion
-
-        #region Properties
-
-        // Public
-
-        // Private
-
-        // Override
-
-
-        #endregion
-
-        #region Constructors
-
-        // Public
-        public ColumnSettings()
-        {
-
-        }
-
-
-        // Private
-
-
-        #endregion
-
-        #region Events
-
-        // Public
-
-        // Private
-
-        // Override
-
-
-        #endregion
-
-        #region Methods
-
-        // Public
         public void Create(String filePath)
         {
             var workbook = new XLWorkbook();
@@ -69,12 +20,5 @@ namespace ClosedXML.Examples.Columns
 
             workbook.SaveAs(filePath);
         }
-
-        // Private
-
-        // Override
-
-
-        #endregion
     }
 }
