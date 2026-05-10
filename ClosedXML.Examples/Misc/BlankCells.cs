f53b88a16872bf10153cda75d02e6dcb531bd519
jahav <havlicek.honza@gmail.com>
2026-05-10 22:22:56 +0200

    Update example test file BlankCells
    Update reference test file for example BlankCells. After the style
    infrastructure switch, the styles part is saved differently.
    No semantic change to the styles part.

--- ClosedXML.Examples/Misc/BlankCells.cs
+++ ClosedXML.Examples/Misc/BlankCells.cs
@@ -1,45 +1,10 @@
 using System;
 using ClosedXML.Excel;
 
-
 namespace ClosedXML.Examples.Misc
 {
     public class BlankCells : IXLExample
     {
-        #region Variables
-
-        // Public
-
-        // Private
-
-
-        #endregion
-
-        #region Properties
-
-        // Public
-
-        // Private
-
-        // Override
-
-
-        #endregion
-
-        #region Events
-
-        // Public
-
-        // Private
-
-        // Override
-
-
-        #endregion
-
-        #region Methods
-
-        // Public
         public void Create(String filePath)
         {
             var wb = new XLWorkbook();
@@ -48,12 +13,5 @@ namespace ClosedXML.Examples.Misc
             ws.Cell(1, 1).Clear();
             wb.SaveAs(filePath);
         }
-
-        // Private
-
-        // Override
-
-
-        #endregion
     }
 }
