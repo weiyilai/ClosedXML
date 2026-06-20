7c3085d184cebbd81e3adabc64deced426a94789
jahav <havlicek.honza@gmail.com>
2026-06-20 17:51:53 +0200

    chore: Convert to file namespace

--- ClosedXML/Excel/XLCellsUsedOptions.cs
+++ ClosedXML/Excel/XLCellsUsedOptions.cs
@@ -1,27 +1,24 @@
-#nullable disable
-
 using System;
 
-namespace ClosedXML.Excel
+namespace ClosedXML.Excel;
+
+[Flags]
+public enum XLCellsUsedOptions
 {
-    [Flags]
-    public enum XLCellsUsedOptions
-    {
 
-        None                    = 0,
-        NoConstraints           = None,
+    None = 0,
+    NoConstraints = None,
 
-        Contents                = 1 << 0,
-        DataType                = 1 << 1,
-        NormalFormats           = 1 << 2,
-        ConditionalFormats      = 1 << 3,
-        Comments                = 1 << 4,
-        DataValidation          = 1 << 5,
-        MergedRanges            = 1 << 6,
-        Sparklines              = 1 << 7,
+    Contents = 1 << 0,
+    DataType = 1 << 1,
+    NormalFormats = 1 << 2,
+    ConditionalFormats = 1 << 3,
+    Comments = 1 << 4,
+    DataValidation = 1 << 5,
+    MergedRanges = 1 << 6,
+    Sparklines = 1 << 7,
 
-        AllFormats = NormalFormats | ConditionalFormats,
-        AllContents = Contents | DataType | Comments,
-        All = Contents | DataType | NormalFormats | ConditionalFormats | Comments | DataValidation | MergedRanges | Sparklines
-    }
+    AllFormats = NormalFormats | ConditionalFormats,
+    AllContents = Contents | DataType | Comments,
+    All = Contents | DataType | NormalFormats | ConditionalFormats | Comments | DataValidation | MergedRanges | Sparklines
 }
